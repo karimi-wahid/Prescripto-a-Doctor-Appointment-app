c275b2cfa054eda89c5f12ef021c73a86950e9dc
Wahidullah Karimi <karimi.wahidullah22@gmail.com>
2025-01-07 09:50:42 +0430

    Done with Contact and appointment pages.

--- src/components/RelatedDoctors.jsx
+++ src/components/RelatedDoctors.jsx
@@ -1,20 +1,29 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AppContext } from "../context/AppContext";
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
-const TopDoctors = () => {
-  const navigate = useNavigate();
-  const { doctors } = useContext(AppContext);
+const RelatedDoctors = ({docId, speciality}) => {
+  const {doctors} = useContext(AppContext)
+  const [relDoc, setRelDoc] = useState([])
+  const navigate = useNavigate()
+
+  useEffect(()=> {
+    if (doctors.length > 0 && speciality) {
+      const doctorsData = doctors.filter((doc) => doc.speciality === speciality && doc._id !== docId);
+      setRelDoc(doctorsData)
+    }
+
+  },[doctors, speciality, docId])
   return (
     <section className="flex flex-col items-center gap-4 my-16 to-gray-900 md:mx-10">
-      <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+      <h1 className="text-3xl font-medium">Related Doctors</h1>
       <p className="text-center text-sm">
         Simply brose thorugh our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 8).map((item, index) => (
+        {relDoc.slice(0, 5).map((item, index) => (
           <div
-          onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
+            onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}>
             <img className="bg-blue-50" src={item.image} alt={item.name} />
@@ -38,7 +47,7 @@ const TopDoctors = () => {
         More
       </button>
     </section>
-  );
-};
+  )
+}
 
-export default TopDoctors;
+export default RelatedDoctors
